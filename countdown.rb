7ebfe9143f3553b2aaa51ea8962ff6532a5c4ae7
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-11-20 03:03:41 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number-- 
+  end
+  return "HAPPY NEW YEAR!"
+    
+  
+end
+
+
+def countdown_with_sleep(number)
+  sleep(num_secs)
 end
