84e3bf01f79560808b9b20272ac7f0b25fe448f4
Kudzanayi Dzvairo <kudzanayi@Kudzanayis-MacBook-Air.local>
2018-11-27 14:58:38 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,19 +1,18 @@
 #write your code here
 
-def countdown(number)
-
-while number > 0
-  puts "#{number} SECOND(S)"
-  number -= 1
+def countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
-  puts "Happy New Year"
-end
 
-def countdown_with_sleep(num)
-    one_sec = sleep 1
-    while num > 0
-     puts "#{num}"
-     num -= one_sec
-   end
-   return "Goodnight"
-end 
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
